088aec02f7648db40a82acf8ea0365d6c156e4fc
shuangfu <songshuangfu@gmail.com>
2024-02-23 14:40:11 +0800

    add bin voc to speed up the startup

---
+++ run.sh
@@ -0,0 +1,3 @@
+# "Usage: ./mono_kitti path_to_vocabulary path_to_settings path_to_sequence"
+#KITTI00-02 03 04-12
+./Examples/Monocular/mono_kitti Vocabulary/ORBvoc.bin ./Examples/Monocular/KITTI04-12.yaml /media/autolab/ssf/KITTI/color_data_odometry/sequences/07
